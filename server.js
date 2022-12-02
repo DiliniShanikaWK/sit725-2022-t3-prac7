f45d8eae8dcf82f4f8fff312366827c5eef7d6ca
DiliniShanikaWK <S220053052@deakin.edu.au>
2022-12-02 20:05:31 +1100

    Practical 3 commited

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require("express")
 var app = express();
 
-app.use(express.static(__dirname+ 'public'));
+app.use(express.static(__dirname+ '/public'));
 app.use(express.json());
-app.use(express.urlencoded({extends: false}));
+app.use(express.urlencoded({ extended: true }))
 
-const addNumbers = (number1, number2) =>{
+/*const addNumbers = (number1, number2) =>{
  var num1 = parseInt(number1);
  var num2 = parseInt(number2);
  var result  = num1+num2;
@@ -16,7 +16,7 @@ app.get("/addTwoNumbers",(req,res) => {
  let { number1, number2 } = req.query;
  let result = addNumbers(number1, number2);
  res.json({ status: 200, data: result, message: 'success'})
-})
+})*/
 
 var port = process.env.port || 3000;
 
