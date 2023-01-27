507311e50067a0c516b7139912205ad526d9c028
DiliniShanikaWK <S220053052@deakin.edu.au>
2023-01-27 22:38:10 +1100

    Pactical 7 commited

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var cors = require("cors");
 var projectCollection;
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
@@ -39,6 +41,16 @@ app.get("/addTwoStrings/:firstName/:secondName", function (req, res, next) {
   } else {
     res.json({ result: result, statusCode: 200 }).status(200);
   }
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  setInterval(() => {
+    socket.emit("number", parseInt(Math.random() * 10));
+  }, 1000);
 });
 
 //mongodb connection
@@ -99,7 +111,7 @@ app.get("/api/projects", (req, res) => {
 
 var port = process.env.port || 3000;
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log("App listening to http://localhost: " + port);
   createCollection("Cars");
 });
